0df57474e92eb8fe1e231b7c3073671367f9ffcc
0200832311032 <0200832311032@LAB03-13>
2025-09-05 21:49:51 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppLogin/App.xaml.cs
@@ -0,0 +1,44 @@
+﻿
+namespace MauiAppLogin
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            string? usuario_logado = null;
+
+            MainPage = new Login();
+
+            Task.Run(async () =>
+            {
+                usuario_logado = await SecureStorage.Default.GetAsync("usuario_logado");
+
+                if(usuario_logado == null)
+                {
+                    MainPage = new Login();
+                }
+                else
+                {
+                    MainPage = new Protegida();
+                }
+            });
+        } // Fecha método construtor App
+
+        protected override Window CreateWindow(IActivationState activationState)
+        {
+            var window = base.CreateWindow(activationState);
+
+            window.Width = 400;
+            window.Height = 600;
+
+            if (this.MainPage == null)
+            {
+                this.MainPage = new MainPage();
+            }
+
+            return window;
+        } // Fecha o método
+    }
+}
